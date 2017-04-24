4ca2bc9315ecbf8eccde8e574a70d0e54604fab4
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2017-04-24 13:22:12 +0200

    Oprettet Vaccine-Class for Client SIde. Oprettet properties og Override String, samt lavet constructor.

---
+++ Vaccine App/Model/Vaccine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaccine_App.Model
+{
+    class Vaccine
+    {
+        public string Navn { get; set; }
+        public string Info { get; set; }
+
+        public Vaccine(string Navn, string Info)
+        {
+            this.Navn = Navn;
+            this.Info = Info;
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
