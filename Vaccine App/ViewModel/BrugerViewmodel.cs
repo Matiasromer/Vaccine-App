40c6b21df3ba04eea387183cba3dc74b4500c18f
Matias <Matias@DESKTOP-TA0TGEP>
2017-04-27 13:30:12 +0200

    tilføjet værdier i brugerviewmodel (barncpr og navn) samt tilføjet handler og singleton i brugerviewmodel metoden

--- Vaccine App/ViewModel/BrugerViewmodel.cs
+++ Vaccine App/ViewModel/BrugerViewmodel.cs
@@ -1,13 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Vaccine_App.Handler;
+using Vaccine_App.Model;
 
 namespace Vaccine_App.ViewModel
 {
-    class BrugerViewmodel
+    class BrugerViewmodel : INotifyPropertyChanged
     {
+        public BrugerSingleton BrugerSingleton { get; set; }
+        public VaccineSingleton VaccineSingleton { get; set; }
+        //Commands
+        public ICommand CreateBarnCommand { get; set; }
+        public ICommand DeleteBarnCommand { get; set; }
 
+
+        //Handler
+
+        private int barncpr;
+
+        public int Barncpr
+        {
+            get { return barncpr; }
+            set { barncpr = value; }
+        }
+
+        private string barnnavn;
+
+        public string BarnNavn
+        {
+            get { return barnnavn; }
+            set { barnnavn = value; }
+        }
+
+        public Handler.BrugerHandler BrugerHandler;
+
+        //ViewModel
+        public BrugerViewmodel()
+        {
+            BrugerHandler = new Handler.BrugerHandler(this);
+            BrugerSingleton = BrugerSingleton.Instance;
+        }
+
+    
+
+        //INotifyPropChanged interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
