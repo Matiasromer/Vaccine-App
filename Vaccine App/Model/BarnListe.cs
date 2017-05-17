294c9b6866caedb5150cf462b70ebd451bd72d99
Jonatan <Jonatan@DESKTOP-3SG699B>
2017-05-17 13:52:31 +0200

    udkommenteret noget bras!

---
+++ Vaccine App/Model/BarnListe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaccine_App.Model
+{
+    class BarnListe
+    {
+        //Properties
+        public string Barn_Navn;
+
+        public int Barn_Id { get; set; }
+
+        public string Info;
+
+        //Constructor
+        public BarnListe(string BarnNavn, int BarnId, string Info)
+        {
+            this.Barn_Navn = BarnNavn;
+            this.Barn_Id = BarnId;
+            this.Info = Info;
+        }
+
+        //Override string. Format data hentes ind i.
+        public override string ToString()
+        {
+            return $"{Barn_Id}   -  {Barn_Navn} -  {Info}  ";
+        }
+    }
+}
--- Vaccine App/Model/Kalender.cs
+++ Vaccine App/Model/Kalender.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using Vaccine_App.ViewModel;
+using Vaccine_App.Model;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage.AccessCache;
+using System.Collections.ObjectModel;
 
 namespace Vaccine_App.Model
 {
@@ -16,81 +19,107 @@ namespace Vaccine_App.Model
         //Constructor
         public Kalender(string VaccineNavn, DateTime DateTime)
         {
+            //barnListe = new ObservableCollection<BarnListe>(Liste());
+            //SelectedBarnListe = barnListe.FirstOrDefault();
             this.VaccineNavn = VaccineNavn;
             this.Date = DateTime;
         }
 
-        public List<string> Liste()
-        {
+        //    public ObservableCollection<BarnListe> barnListe { get; set; }
 
-            List<string> vacciner = new List<string>();
-            vacciner.Add("");
-            
-            return vacciner;
-        }
-        //Liste over vaccinationer:
-       
-        
-        
+        //    private BarnListe selectedBarnListe;
 
-        
+        //    public BarnListe SelectedBarnListe
+        //    {
+        //        get { return selectedBarnListe; }
+        //        set { selectedBarnListe = value; }
+        //    }
+
+        //    public List<BarnListe> Liste()
+        //    {
+
+        //        List<BarnListe> vacciner = new List<BarnListe>();
+        //        vacciner.Add(HepatitisB1());
+        //        vacciner.Add(HepatitisB2());
+        //        vacciner.Add(DiTeKiPolHib1());
+        //        vacciner.Add(Pnoumokok1());
+        //        vacciner.Add(DiTeKiPolHib2());
+        //        vacciner.Add(Pnoumokok2());
+        //        vacciner.Add(HepatitisB3());
+        //        vacciner.Add(DiTeKiPolHib3());
+        //        vacciner.Add(Pnoumokok3());
+        //        vacciner.Add(MFR1());
+        //        vacciner.Add(DiTeKiPolHib4());
+        //        vacciner.Add(Hpv());
+        //        vacciner.Add(MFR2());
+
+        //        return vacciner;
+        //    }
+        //    Liste over vaccinationer:
 
 
-        //Vacciner, og forskellige anal måneder der skal gå før de skal modtages - inkluderer navn, nr. og Antal måneder fra foedsel.
-        #region Vacciner
-        public string HepatitisB1()
-        {
-            return "Hepatitis B Vaccination 1, 1 Måned";
-        }
-        public string HepatitisB2()
-        {
-            return "Hepatitis B Vaccination 2, 2 Måneder";
-        }
-        public string DiTeKiPolHib1()
-        {
-            return "Di-Te-Ki-Pol-hib vaccination 1, 3 Måneder";
-        }
-        public string Pnoumokok1()
-        {
-            return "Pnoumokok vaccination 1, 3 Måneder";
-        }
-        public string DiTeKiPolHib2()
-        {
-            return "Di-Te-Ki-Pol-hib vaccination 2, 5 Måneder";
-        }
-        public string Pnoumokok2()
-        {
-            return "Pnoumokok vaccination 2, 5 Måneder";
-        }
-        public string HepatitisB3()
-        {
-            return "Hepatitis B Vaccination 3, 12 Måneder";
-        }
-        public string DiTeKiPolHib3()
-        {
-            return "Di-Te-Ki-Pol-hib vaccination 3, 12 Måneder";
-        }
-        public string Pnoumokok3()
-        {
-            return "Pnoumokok vaccination 3, 12 Måneder";
-        }
-        public string MFR1()
-        {
-            return "MFR vaccination 1, 12 Måneder";
-        }
-        public string DiTeKiPolHib4()
-        {
-            return "Di-Te-Ki-Pol-hib vaccination 4, 60 Måneder";
-        }
-        //Valgfri Vaccination for piger
-        public string Hpv()
-        {
-            return "Hpv Vaccinaion, 144 Måneder";
-        }
-        public string MFR2()
-        {
-            return "MFR vaccination 2, 144 Måneder";
-        }
-        #endregion
+
+
+
+
+
+        //    Vacciner, og forskellige anal måneder der skal gå før de skal modtages - inkluderer navn, nr. og Antal måneder fra foedsel.
+        //    #region Vacciner
+        //    public string HepatitisB1(BarnListe)
+        //    {
+        //        return HepatitisB1(new BarnListe("Brian", 1, "Hepatitis B, 1 Måned"));
+
+        //    }
+        //    public string HepatitisB2(BarnListe)
+        //    {
+        //        return HepatitisB2(new BarnListe("Brian", 1, "Hepatitis B, 2 måneder"));
+        //    }
+        //    public string DiTeKiPolHib1(BarnListe)
+        //    {
+        //        return DiTeKiPolHib1(new BarnListe("Brian", 1, "Di-Te-Ki-Pol-hib, 3 Måneder"));
+        //    }
+        //    public string Pnoumokok1(BarnListe)
+        //    {
+        //        return Pnoumokok1(new BarnListe("Brian", 1, "Pnoumokok, 3 Måneder"));
+        //    }
+        //    public string DiTeKiPolHib2(BarnListe)
+        //    {
+        //        return DiTeKiPolHib2(new BarnListe("Brian", 1, "Di-Te-Ki-Pol-hib, 5 Måneder"));
+        //    }
+        //    public string Pnoumokok2(BarnListe)
+        //    {
+        //        return Pnoumokok2(new BarnListe("Brian", 1, "Pnoumokok, 5 Måneder"));
+        //    }
+        //    public string HepatitisB3(BarnListe)
+        //    {
+        //        return DiTeKiPolHib3(new BarnListe("Brian", 1, "Hepatitis B, 12 Måneder"));
+        //    }
+        //    public string DiTeKiPolHib3(BarnListe)
+        //    {
+        //        return DiTeKiPolHib3(new BarnListe("Brian", 1, "Di-Te-Ki-Pol-hib, 12 Måneder"));
+        //    }
+        //    public string Pnoumokok3(BarnListe)
+        //    {
+        //        return Pnoumokok3(new BarnListe("Brian", 1, "Pnoumokok, 12 Måneder"));
+        //    }
+        //    public string MFR1(BarnListe)
+        //    {
+        //        return MFR1(new BarnListe("Brian", 1, "MFR, 12 Måneder"));
+        //    }
+        //    public string DiTeKiPolHib4(BarnListe)
+        //    {
+        //        return DiTeKiPolHib4(new BarnListe("Brian", 1, "Di-Te-Ki-Pol-hib vaccination 4, 60 Måneder"));
+        //    }
+        //    Valgfri Vaccination for piger
+        //    public string Hpv(BarnListe)
+        //    {
+        //        return Hpv(new BarnListe("Brian", 1, "Hpv, 144 Måneder"));
+        //    }
+        //    public string MFR2(BarnListe)
+        //    {
+        //        return MFR2(new BarnListe("Brian", 1, "MFR, 144 Måneder"));
+        //    }
+        //    #endregion
+        //}
     }
 }
